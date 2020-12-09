6059cc869d8f99aa89d545356959410fb12b6f19
Lia <judykim257@gmail.com>
2020-12-09 13:55:46 +0900

    step-1: 단어 밀어내기 구현
    step-2 친화적으로 설계하여 알고리즘은 다소 복잡할 수 있음

--- Rubik's Cube/Rubik's Cube/main.swift
+++ Rubik's Cube/Rubik's Cube/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
+// STEP 1: 단어 밀어내기
+print("enter a word, an integer (-100 <= N < 100), L or R")
+print("ex) apple 3 L \n\ninput: ", terminator:"")
 
+let input1 = readLine()!
+var test1 = step1(input1)
+test1.calString()
+print(test1.pushString())
 
+// ***********************************************************
